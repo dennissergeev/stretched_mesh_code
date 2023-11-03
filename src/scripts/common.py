bf8242ba63073054e5cfdf38176bc123dee38ab7
Denis Sergeev <dennis.sergeev@gmail.com>
2023-11-03 16:06:30 +0000

    add a NB for plotting mesh geometry

---
+++ src/scripts/common.py
@@ -0,0 +1,19 @@
+"""Common objects stretched_mesh_proj."""
+SIMULATIONS = {
+    "c192": {
+        "title": "C192",
+        "long_name": "thai_hab1_c192s10",  # change
+        "planet": "hab1",
+        "kw_plt": {"color": "C0"},
+        "timestep": 300,
+        "stretch_factor": 1,
+    },
+    "c192s10": {
+        "title": "C192s10",
+        "long_name": "thai_hab1_c192s10",
+        "planet": "hab1",
+        "kw_plt": {"color": "C1"},
+        "timestep": 150,
+        "stretch_factor": 10,
+    },
+}
