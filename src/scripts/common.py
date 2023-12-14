5da12684a232197ae0e76e6e19900671382e37e5
Denis Sergeev <dennis.sergeev@gmail.com>
2023-12-14 10:36:27 +0000

    remove pp_lfric_data script and add Regrid NB instead, update common definitions

--- src/scripts/common.py
+++ src/scripts/common.py
@@ -11,6 +11,7 @@ Simulation = NamedTuple(
         ("work_name", str),
         ("planet", tuple),
         ("timestep", int),
+        ("days_per_job", int),
         ("stretch_factor", float),
         ("vert_lev", str),
         ("kw_plt", dict),
@@ -21,11 +22,12 @@ Simulation = NamedTuple(
 SIMULATIONS = {
     "c192s10e": Simulation(
         title="C192s10e",
-        work_name="thai_hab1_c192s10",
+        work_name="thai_hab1_c192s10_l63_ec",
         planet="hab1",
         timestep=120,
+        days_per_job=5,
         stretch_factor=10,
-        vert_lev="L38_29t_9s_40km",
+        vert_lev="L63_50t_13s_40km",
         kw_plt={"color": "C0", "marker": "h"},
     ),
     "c192s10r": Simulation(
@@ -33,6 +35,7 @@ SIMULATIONS = {
         work_name="thai_hab1_c192s10_l63_rc",
         planet="hab1",
         timestep=120,
+        days_per_job=5,
         stretch_factor=10,
         vert_lev="L63_50t_13s_40km",
         kw_plt={"color": "C1", "marker": "p"},
@@ -42,6 +45,7 @@ SIMULATIONS = {
         work_name="thai_hab1_c192s10_l63_pc",
         planet="hab1",
         timestep=120,
+        days_per_job=5,
         stretch_factor=10,
         vert_lev="L63_50t_13s_40km",
         kw_plt={"color": "C2", "marker": "s"},
@@ -51,12 +55,13 @@ SIMULATIONS = {
         work_name="thai_hab1_c192_l63",
         planet="hab1",
         timestep=300,
+        days_per_job=10,
         stretch_factor=1,
         vert_lev="L63_50t_13s_40km",
         kw_plt={"color": "C3", "marker": "o"},
     ),
 }
 
-
+SPINUP_DAYS = 500
 DAYSIDE = Region(-90, 90, -90, 90, name="dayside", model=lfric)
 NIGHTSIDE = Region(90, -90, -90, 90, name="nightside", model=lfric)
